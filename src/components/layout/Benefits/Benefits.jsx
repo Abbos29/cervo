ee0f3037a17e46f4836980dbf0474009800b7b9b
BoburXO <boburhatamov7@gmail.com>
2025-05-19 18:22:00 +0300

    feat: full static pages

--- src/components/layout/Benefits/Benefits.jsx
+++ src/components/layout/Benefits/Benefits.jsx
@@ -1,53 +1,31 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import s from './Benefits.module.scss'
 import Container from '@/components/ui/Container/Container'
 import Pretitle from '@/components/ui/Pretitle/Pretitle'
 import SectionTitle from '@/components/ui/SectionTitle/SectionTitle'
-
-const benefitsData = [
-    {
-        icon: '/img/benefits-icon-1.svg',
-        title: 'Wide Range of Products',
-        text: 'A variety of car rims, tires and batteries for all types of vehicles',
-    },
-    {
-        icon: '/img/benefits-icon-2.svg',
-        title: 'Guaranteed Quality',
-        text: 'Certified products from trusted manufacturers',
-    },
-    {
-        icon: '/img/benefits-icon-3.svg',
-        title: 'International Delivery',
-        text: 'Fast and reliable shipping to partners worldwide',
-    },
-    {
-        icon: '/img/benefits-icon-4.svg',
-        title: 'Personal Manager for Clients',
-        text: 'Individual approach and prompt solutions to any tasks',
-    },
-    {
-        icon: '/img/benefits-icon-5.svg',
-        title: '24/7 Customer Support',
-        text: 'Professional assistance anytime, anywhere',
-    },
-    {
-        icon: '/img/benefits-icon-6.svg',
-        title: 'Profitable Wholesale Purchases',
-        text: 'Exclusive discounts and offers for wholesale clients',
-    },
-]
+import { useIsClient } from 'usehooks-ts'
 
 const Benefits = () => {
+    const { t } = useTranslation()
+    const isClient = useIsClient()
+
+    const cards = t('benefits.cards', { returnObjects: true })
+
+    const benefitsData = Array.isArray(cards)
+        ? cards.map((card, index) => ({
+            ...card,
+            icon: `/img/benefits-icon-${index + 1}.svg`,
+        }))
+        : []
+
     return (
         <section className={s.benefits}>
-            <Container>
+            {isClient && <Container>
                 <div className={s.top}>
-                    <Pretitle>Benefits</Pretitle>
-                    <SectionTitle white>Advantages of the Company</SectionTitle>
-                    <p>
-                        We manufacture car batteries, wheels, and tires and are looking for distributors.
-                        Flexible terms, reliable delivery, and support.
-                    </p>
+                    <Pretitle>{t('benefits.pretitle')}</Pretitle>
+                    <SectionTitle white>{t('benefits.title')}</SectionTitle>
+                    <p>{t('benefits.description')}</p>
                 </div>
                 <div className={s.wrapper}>
                     {benefitsData.map((benefit, index) => (
@@ -58,7 +36,7 @@ const Benefits = () => {
                         </div>
                     ))}
                 </div>
-            </Container>
+            </Container>}
         </section>
     )
 }
