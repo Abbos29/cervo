1303b4608070ec0d1af6e6a82b890f8d0198f9f8
Abbos29 <abbosvaliev2003@gmail.com>
2025-06-04 22:57:57 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cervo.global"
+      },
+      {
+        protocol: "http",
+        hostname: "localhost"
+      },
+      {
+        protocol: "https",
+        hostname: "cervo.pythonanywhere.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
