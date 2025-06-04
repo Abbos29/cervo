141e65d70ac17b24255cecd1fac0fbef5808b93b
BoburXO <boburhatamov7@gmail.com>
2025-06-04 18:34:38 +0300

    feat: home api's

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,20 @@ import ContactsWrap from '@/components/layout/ContactsWrap/ContactsWrap'
 import Hero from '@/components/layout/Hero/Hero'
 import Numbers from '@/components/layout/Numbers/Numbers'
 import HeadSeo from '@/components/ui/HeadSeo/HeadSeo'
+import { axiosInstance } from '@/utils/axios'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useIsClient } from 'usehooks-ts'
 
-const Home = () => {
+
+export const getServerSideProps = async () => {
+  const { data: numbers } = await axiosInstance.get(`/numbers`)
+  const { data: benefits } = await axiosInstance.get(`/benefits`)
+  const { data: socials } = await axiosInstance.get(`/socials`)
+  return { props: { numbers, benefits, socials } }
+}
+
+const Home = ({ numbers, benefits, socials }) => {
   const { t } = useTranslation()
   const isClient = useIsClient()
   return (
@@ -15,9 +24,9 @@ const Home = () => {
       {isClient && <>
         <HeadSeo title={t("pages.main")} />
         <Hero />
-        <Numbers />
-        <Benefits />
-        <ContactsWrap />
+        <Numbers numbers={numbers} />
+        <Benefits benefits={benefits} />
+        <ContactsWrap socials={socials} />
       </>
       }
     </>
